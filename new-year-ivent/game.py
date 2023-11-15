7566bd9e05182735f0622e61d30b59823e58316d
Andrey Nikel <nikelandrey94@gmail.com>
2023-11-15 16:45:19 +0600

    пробный запуск

--- new-year-ivent/game.py
+++ new-year-ivent/game.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3.12
 
-import tkinter as tk
+from tkinter import *
 
 
 def main():
-    root = tk.Tk()
+    root = Tk()
+    bg = PhotoImage(file="background1.png")
+    bg_label_main = Label(root, image=bg)
+    bg_label_main.place(x=0, y=0)
+    label_welcome = Label(root, text="Welcome")
+    label_welcome.pack(pady=50)
+    frame1 = Frame(root)
+    frame1.pack(pady=20)
+    root.title("Новогодняя акция!!!")
+    root.geometry("1000x1000")
     root.mainloop()
     return
 
