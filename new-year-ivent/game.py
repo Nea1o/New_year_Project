629625f2b532b8d4a46c836c5cc4f60fee9b43ac
Andrey Nikel <nikelandrey94@gmail.com>
2023-11-29 15:42:31 +0600

    create dict

--- new-year-ivent/game.py
+++ new-year-ivent/game.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3.12
 import tkinter
+from tkinter import ttk
 import random
 
 
 def main():
+    result_dict = {}
+
+    def show_message(*event):
+        def rand_int():
+            point = random.choice([100, 100, 100, 100, 100, 150, 150, 150, 200, 300])
+            return point
+
+        label["text"] = entry.get()
+        number_card = label["text"]
+        result_dict[number_card] = result_dict.setdefault(number_card, 0) + rand_int()
+        print(result_dict)
+
     root = tkinter.Tk()
     bg = tkinter.PhotoImage(file="background1.png")
     bg_label_main = tkinter.Label(root, image=bg)
@@ -14,19 +27,18 @@ def main():
     frame1.pack(pady=20)
     root.title("Новогодняя акция!!!")
     root.geometry("800x800")
+
+    entry = ttk.Entry()
+    entry.place(height=30, width=100, x=350, y=90)
+
+    label = ttk.Label()
+    label.place(height=30, width=100, x=350, y=650)
+
     bt1 = tkinter.Button(text="Click me!1")
-    bt1.place(height=100, width=200, x=100, y=100)
-    bt1.bind("<ButtonPress-1>", rand_int)
-    bt2 = tkinter.Button(text="Click me!2")
-    bt2.place(height=100, width=200, x=300, y=100)
-    bt3 = tkinter.Button(text="Click me!3")
-    bt3.place(height=100, width=200, x=500, y=100)
+    bt1.place(height=100, width=200, x=100, y=200)
+    bt1.bind("<ButtonPress-1>", show_message)
+
     root.mainloop()
-    return
-
-
-def rand_int(event):
-    print(random.choice([100, 100, 100, 100, 100, 150, 150, 150, 200, 300]))
     return
 
 
